8ef011f282b152a43965759711d823d86d7e2494
Alexandra Janin <alexandra.janin@makina-corpus.com>
2016-07-28 12:31:23 +0200

    Remove dist folder

--- dist/js/utils.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  this.getObjectIdFromURI = function(uri) {
-    var splitted_uri;
-    splitted_uri = uri.split("/");
-    return splitted_uri[splitted_uri.length - 1];
-  };
-
-}).call(this);
